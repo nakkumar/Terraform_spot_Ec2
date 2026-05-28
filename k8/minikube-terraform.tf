ffc3d4bd7c48e4dc2cc50a9dada61074d984d54a
Arun kumar <61176789+nakkumar@users.noreply.github.com>
2026-05-28 14:01:32 +0530

    update volume and userdata separation

--- k8/minikube-terraform.tf
+++ k8/minikube-terraform.tf
@@ -16,63 +16,21 @@ resource "aws_instance" "TF-test"{
     subnet_id = "subnet-033bd3dde8b3ed93a"
     key_name = "MT5-DEMO"
     vpc_security_group_ids = ["sg-063db659abab09ea8"]
-    user_data = base64encode(<<-EOF
-                                #!/bin/bash
-                                cd /home/ubuntu
+    user_data_base64 = base64encode(file("userdata.sh"))
 
-                                ########################################
-                                # Install kubectl
-                                ########################################
+    root_block_device {
 
-                                curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-                                chmod +x kubectl
-                                mv kubectl /usr/local/bin/kubectl
+      volume_size = 30
+      volume_type = "gp2"
 
-                                ########################################
-                                # Install Docker
-                                ########################################
+      delete_on_termination = true
+   }
 
-                                curl -fsSL https://get.docker.com -o get-docker.sh
-                                chmod +x get-docker.sh
-                                sh get-docker.sh
-                                systemctl enable --now docker
-
-                                ########################################
-                                # Add ubuntu user to docker group
-                                ########################################
-
-                                usermod -aG docker ubuntu
-
-                                ########################################
-                                # Install Minikube
-                                ########################################
-
-                                curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-                                chmod +x minikube-linux-amd64
-                                mv minikube-linux-amd64 /usr/local/bin/minikube
-
-                                ########################################
-                                # Create required directories
-                                ########################################
-
-                                mkdir -p /home/ubuntu/.kube
-                                mkdir -p /home/ubuntu/.minikube
-
-                                ########################################
-                                # Set ownership
-                                ########################################
-
-                                chown -R ubuntu:ubuntu /home/ubuntu/.kube
-                                chown -R ubuntu:ubuntu /home/ubuntu/.minikube
-
-                                ########################################
-                                # Start Minikube as ubuntu user
-                                ########################################
-
-                                sudo -u ubuntu minikube start --driver=docker
-              EOF
-    )
   tags = {
     Name = "MiniKube-k8"
   }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.TF-test.public_ip
 }
